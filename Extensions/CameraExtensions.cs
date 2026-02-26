b9e45d0e4bc098019d673b81ab2318931629c797
Erwan Michot <erwan@voulez-vous.studio>
2026-02-26 15:38:03 +0100

    project 3D box to screen rect on CameraExtensions

--- Extensions/CameraExtensions.cs
+++ Extensions/CameraExtensions.cs
@@ -18,5 +18,25 @@ namespace VV.Utility
         {
             return cam.ScreenPointToRay(position).GetIntersection(new Plane(planeNormal, planeOrigin));
         }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Rect BoxToScreenRect(this Camera cam, Vector3 center, Vector3 size)
+        {
+            Bounds bounds = new Bounds(center, size);
+
+            Vector2 min = Vector2.positiveInfinity;
+            Vector2 max = Vector2.negativeInfinity;
+
+            bounds.ForEachVertices(vertex =>
+            {
+                Vector3 p = cam.WorldToViewportPoint(vertex);
+                if (p.z < 0) return;
+
+                min = new Vector2(Mathf.Min(p.x, min.x), Mathf.Min(p.y, min.y));
+                max = new Vector2(Mathf.Max(p.x, max.x), Mathf.Max(p.y, max.y));
+            });
+
+            return new Rect(min.x , min.y, max.x - min.x, max.y - min.y);
+        }
     }
 }
